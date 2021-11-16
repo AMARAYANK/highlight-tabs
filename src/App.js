9861cff2e025e88e03ca76080f69fd2586b723a6
amarayank <amarayank4@gmail.com>
2021-11-16 13:56:53 +0530

    added active-tab styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 import Tab from './Tab';
-import { Link } from "react-router-dom";
+import {  Link, useMatch, useResolvedPath } from "react-router-dom";
 
+
+
+
+function CustomLink({ children, to, ...props }) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{ background: match ? "#452d88" : "none", color: match ? "#eee" : "none" }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
 
 function App({children}) {
   
@@ -10,24 +29,26 @@ function App({children}) {
     <div className="app">
       <div className="browser">
         <div className="tabs">
-        <Tab>
-          <Link to="/Home">Home</Link>
-        </Tab>
-        <Tab>
-         <Link to="/Features">Features</Link>
-        </Tab>
-        <Tab>
-         <Link to="/About">About</Link>
-        </Tab>
+          <Tab>
+            <CustomLink to="/Home">Home</CustomLink>
+          </Tab>
+
+          <Tab>
+          <CustomLink to="/Features">Features</CustomLink>
+          </Tab>
+          <Tab>
+            <CustomLink to="/About">About</CustomLink>
+          </Tab>
       </div>
-      <div className="viewport">
-       {children}
+        <div className="viewport">
+        {children}
+        </div>  
       </div>
-    </div>
     </div>
   
     
   );
 }
+
 
 export default App;
